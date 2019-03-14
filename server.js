90909a385dbad7e4d00a178602e903bf6fd335ce
Paulyn <paulyntran@live.ca>
2019-03-14 18:14:41 +0000

    Switched from if statements to switch cases

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
-const uuidv4 = require('uuid/v4');
+const uuidv1 = require('uuid/v1');
 const WebSocketServer = require('ws');
 
 // Set the port to 3001
@@ -21,32 +21,36 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', (message) => {
-    let id = uuidv4();
     let obj = JSON.parse(message)
-    if (obj.type === 'postMessage') {
-      let uniqueMessage = JSON.stringify({id: id, username: obj.username, content: obj.content, type: "incomingMessage"});
-      // console.log(obj);
-      // console.log(obj.content);
-      // console.log(obj.username);
 
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocketServer.OPEN) {
-          client.send(uniqueMessage);
-        }
-      });
-    } else if (obj.type === 'postNotification') {
-      let uniqueMessage = JSON.stringify({id: id, username: obj.username, content: obj.content, type: "incomingNotification"});
-    
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocketServer.OPEN) {
-          client.send(uniqueMessage);
-        }
-      })
+    switch(obj.type) {
+      case "postMessage":
+        let uniqueMessage = JSON.stringify({
+          id: uuidv1(), 
+          username: obj.username, 
+          content: obj.content, 
+          type: "incomingMessage"});
+
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocketServer.OPEN) {
+            client.send(uniqueMessage);
+          }
+        });
+        break;
+      case "postNotification":
+        let uniqueNotification = JSON.stringify({
+          id: uuidv1(), 
+          username: obj.username, 
+          content: obj.content, 
+          type: "incomingNotification"});
+      
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocketServer.OPEN) {
+            client.send(uniqueNotification);
+          }
+        });
+        break;
     }
-
-    // console.log(obj.content);
-    // // console.log(`User ${obj.username} said ${obj.content}`);
-    // console.log(obj.username);
 
   });
 
